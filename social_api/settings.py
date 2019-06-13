e30aa01b62dd2451c62b2fda73adfc364c700878
AmmarHazem <ammar.hazem@hotmail.com>
2019-06-13 17:49:35 +0200

    Commit

--- social_api/settings.py
+++ social_api/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'v)*j^38pbtdf2uaw=yuj6ky)b(^jo_8gpr(l01#z759wm@p7w@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['mysocialapi.herokuapp.com']
 
