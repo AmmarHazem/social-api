03ad80d52d7a89f4a9897c183d8090f29021e410
AmmarHazem <ammar.hazem@hotmail.com>
2019-06-13 17:00:31 +0200

    Commit

--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
 # from rest_framework import status
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404
@@ -35,6 +36,7 @@ class ProfileListView(generics.ListAPIView):
 class UserCreateView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserCreateSerializer
+    permission_classes = (AllowAny,)
 
     def perform_create(self, serializer):
         valid_data = serializer.validated_data
