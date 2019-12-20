9002b4667b79696b697e512750e82d7b8fbe6f28
AmmarHazem <ammar.hazem0@gmail.com>
2019-12-20 14:42:32 +0200

    Commit

--- social_api/production.py
+++ social_api/production.py
@@ -1,3 +1,4 @@
+from decouple import config
 from .settings import *
 
 
@@ -10,6 +11,10 @@ SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 X_FRAME_OPTIONS = 'DENY'
+
+INSTALLED_APPS += [
+    'storages',
+]
 
 
 REST_FRAMEWORK = {
@@ -27,5 +32,19 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 10,
 }
 
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = config('S3_BUCKET_NAME')
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com' # '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_LOCATION = 'static'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'social_api/static'),
+]
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 django_heroku.settings(locals())
